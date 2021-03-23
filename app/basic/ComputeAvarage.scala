8cbb9f0d850b194f6cb640792f8e744435d14130
tranttxuan <>
2021-03-24 00:22:02 +0100

    completed ComputeAvarage

--- app/basic/ComputeAvarage.scala
+++ app/basic/ComputeAvarage.scala
@@ -7,6 +7,12 @@ package com.particeep.test.basic
  */
 object ComputeAvarage {
 
-  def average(l: List[Double]) = ???
+  def average(l: List[Double]) = l.foldLeft((0.0, 1))((acc, i) => ((acc._1 + (i - acc._1) / acc._2), acc._2 + 1))._1
+  
+  /* For testing in command line */
+  def main(args: Array[String]) {
+    var list = List[Double](4, 6, 7)
+    println(average(list))
+  }
 
 }
